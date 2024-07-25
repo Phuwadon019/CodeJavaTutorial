cf3dfda10733be376e3ac0e413bfda476de7228d
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-25 14:28:04 +0700

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,16 @@
+public class Operator {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5 + 2 * 4;
+        y = (5 + 2) * 4;
+        System.out.println("x = "+x);
+        System.out.println("x = "+y);
+
+        x = x / 8;
+        System.out.println("x = "+x);
+        
+        float p;
+        p = (float) x/3;
+        System.out.println("o = "+p);
+    }
+}
