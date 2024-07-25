67daab4d9b9160543c64a5556d1ed461d105f356
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-25 14:28:08 +0700

    Create stringdemo.java

---
+++ stringdemo.java
@@ -0,0 +1,13 @@
+public class stringdemo {
+    public static void main(String[] args) {
+        /* วิธีแรก */
+        String greeting;
+        greeting = "Yo what's up bro";
+
+
+        /* วิธีสอง */
+        String name = "Computer";
+
+        System.out.println(greeting + " "+name);
+    }
+}
