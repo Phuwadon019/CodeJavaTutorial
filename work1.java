eb57d5b005af925d84c850ab8f2c120597f2b997
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-25 14:28:15 +0700

    Create work1.java

---
+++ work1.java
@@ -0,0 +1,10 @@
+public class work1 {
+    public static void main(String[] args) {
+        int x,y;
+        x = 62;
+        y = 99;
+        
+        int total = x+y;
+        System.out.println("Total = "+total);
+    }
+}
