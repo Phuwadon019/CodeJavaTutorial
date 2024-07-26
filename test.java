680748927abb3d3eaa954474571fa1b7d03a7c51
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-26 14:47:37 +0700

    Update test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String[] args) {
+        double x = 2.5;
+        double y = -1.5;
+        int m = 18;
+        int n = 4;
+        double pk = m / n + m % n;
+        System.out.println(pk);
+    }
+}
