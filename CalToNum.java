da81520fa329f1aa822926fcfe753f1ea8d7f442
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-31 15:22:27 +0700

    Update CalToNum.java

--- CalToNum.java
+++ CalToNum.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CalToNum {
     public static void main(String[] args) {
-        int number;
+        int number, num1, num2, result;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("____________________");
         System.out.println("1.คำนวณบวกเลข");
@@ -17,12 +17,10 @@ public class CalToNum {
             System.out.println("คำนวณการบวกเลข");
             System.out.println("ใส่ตัวเลขที่คุณต้องการบวกกัน");
             System.out.println("____________________");
-            int num1, num2;
-            Scanner in = new Scanner(System.in);
-            num1 = in.nextInt();
-            num2 = in.nextInt();
-            int sum = num1+num2;
-            System.out.println("ผลลัพธ์ =" +sum);
+            num1 = keyboard.nextInt();
+            num2 = keyboard.nextInt();
+            result = num1+num2;
+            System.out.println("ผลลัพธ์ =" +result);
             System.out.println("____________________");
             break;
             
@@ -31,12 +29,10 @@ public class CalToNum {
             System.out.println("คำนวณการคูณเลข");
             System.out.println("ใส่ตัวเลขที่คุณต้องการบวกกัน");
             System.out.println("____________________");
-            int x, y;
-            Scanner z = new Scanner(System.in);
-            x = z.nextInt();
-            y = z.nextInt();
-            int a = x*y;
-            System.out.println("ผลลัพธ์ =" +a);
+            num1 = keyboard.nextInt();
+            num2 = keyboard.nextInt();
+            result = num1*num2;
+            System.out.println("ผลลัพธ์ =" +result);
             System.out.println("____________________");
             break;
 
@@ -46,12 +42,10 @@ public class CalToNum {
             System.out.println("คำนวณการหารเลข");
             System.out.println("ใส่ตัวเลขที่คุณต้องการบวกกัน");
             System.out.println("____________________");
-            int b, c;
-            Scanner d = new Scanner(System.in);
-            b = d.nextInt();
-            c = d.nextInt();
-            int s = b/c;
-            System.out.println("ผลลัพธ์ =" +s);
+            num1 = keyboard.nextInt();
+            num2 = keyboard.nextInt();
+            result = num1/num2;
+            System.out.println("ผลลัพธ์ =" +result);
             System.out.println("____________________");
             break;
 
@@ -60,12 +54,10 @@ public class CalToNum {
             System.out.println("คำนวณการหารเลข");
             System.out.println("ใส่ตัวเลขที่คุณต้องการบวกกัน");
             System.out.println("____________________");
-            int o, u;
-            Scanner p = new Scanner(System.in);
-            o = p.nextInt();
-            u = p.nextInt();
-            int k = o-u;
-            System.out.println("ผลลัพธ์ =" +k);
+            num1 = keyboard.nextInt();
+            num2 = keyboard.nextInt();
+            result = num1-num2;
+            System.out.println("ผลลัพธ์ =" +result);
             System.out.println("____________________");
             break;
 
