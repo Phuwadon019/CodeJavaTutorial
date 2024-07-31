fb1b36e5ce0278a32d5d4c960414aaf6c4c20f2e
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-07-31 14:50:28 +0700

    Create ifActjava2.java

---
+++ ifActjava2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifActjava2 {
+    public static void main(String[] args) {
+        int score;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your score");
+        score = in.nextInt();
+        if (score >= 60) {
+            System.out.println("your score is " +score+ "result is : pass");
+        }
+        else{
+            System.out.println("your score is " +score+ "result is : pass");
+            
+        }
+    }
+}
