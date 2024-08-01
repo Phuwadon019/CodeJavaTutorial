cef81b5709d1261507ea517b9c04afd00fc7684b
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-08-01 10:22:34 +0700

    Create LoopWhile.java

---
+++ LoopWhile.java
@@ -0,0 +1,13 @@
+public class LoopWhile {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println("result in for loop"+i);
+        }
+        
+        int iwhile = 1;
+        while (iwhile <= 10) {
+            System.out.println("result in while loop"+iwhile);
+            iwhile++ ;
+        }
+    }
+}
