c58ca1226fc4d8991c51f7c65350f00e3f566f01
Phuwadon019 <152456956+Phuwadon019@users.noreply.github.com>
2024-08-01 10:22:38 +0700

    Create result1To100.java

---
+++ result1To100.java
@@ -0,0 +1,10 @@
+public class result1To100 {
+    public static void main(String[] args) {
+        int i = 1, sum = 0;
+        while (i <= 100) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Result 1 To 100 : "+sum);
+    }  
+}
